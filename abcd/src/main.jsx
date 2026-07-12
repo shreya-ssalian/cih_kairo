3f3bd7285ac0b4e3b96926a63df59f5ab3bb2065
Nidhi J Bangera <nidhibangera65@gmail.com>
2026-07-12 08:48:37 +0530

    Delete abcd directory

--- abcd/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SeaPulseApp from './SeaPulse.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SeaPulseApp />
-  </React.StrictMode>,
-)
